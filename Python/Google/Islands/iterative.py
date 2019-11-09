a0be3905848bd0164a609a452a4e072764214556
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2019-11-09 05:02:02 +0000

    Cleaned code, added comments.

--- Python/Google/Islands/iterative.py
+++ Python/Google/Islands/iterative.py
@@ -5,60 +5,78 @@
 
 # Find neighbors
 def find_neighbors(matrix, current, visited):
-    """current is a tuple of (x,y) coordinates."""
-    neighboors = []
+    """Find the neighbors of current that have not being visited.
+    :param matrix: matrix to be used.
+    :param current: tuple of (x,y) coordinates.
+    :param visited: set of visited coordinates."""
+    # Placeholder variables
     m = matrix
     x, y = current
-    print("Finding neighbors of x:" + str(x) + " y: " + str(y))
+    neighbors = []  # Found neighbors
+    # print("Finding neighbors of x:" + str(x) + " y: " + str(y))  # debugging
 
-    # Check top row
-    for i in range(-1, 2):
+    # Iterate through each possible row. Top, Middle, Bottom.
+    for i in range(-1, 2):  # Check column -1, column, column +1
+        # Check top row
         if (x-1 >= 0 and x-1 < len(m) and y+i >= 0 and y+i < len(m[0])):
-            n = (x-1, y+i)  # Neighbor
+            n = (x-1, y+i)  # Generated neighbor
+            # if not visited and equals to 1
             if (n not in visited and m[n[0]][n[1]] == [1]):
-                visited.add(n)
-                neighboors.append(n)
+                # Valid neighbor n
+                visited.add(n)  # Mark as visited
+                neighbors.append(n)  # Add to possible neighbors
+
     for i in range(-1, 2):
+        # Check middle row
         if (x >= 0 and x < len(m) and y+i >= 0 and y+i < len(m[0])):
-            n = (x, y+i)  # Neighbor
+            n = (x, y+i)
             if n not in visited and m[n[0]][n[1]] == [1]:
                 visited.add(n)
-                neighboors.append(n)
+                neighbors.append(n)
+
     for i in range(-1, 2):
+        # Check bottom row
         if (x+1 >= 0 and x+1 < len(m) and y+i >= 0 and y+i < len(m[0])):
-            n = (x+1, y+i)  # Neighbor
+            n = (x+1, y+i)
             if (n not in visited and m[n[0]][n[1]] == [1]):
                 visited.add(n)
-                neighboors.append(n)
-    return neighboors
+                neighbors.append(n)
+    # Return list of valid neighbors
+    return neighbors
 
 
 def getIslands(matrix):
-    islands = 0
-    visited = set()
+    """Get number of islands according to a 2D matrix."""
+    islands = 0  # Total islands
+    visited = set()  # Visited coordinates
+
+    # Iterate through the whole matrix
     for r in range(len(matrix)):
         for c in range(len(matrix[r])):
+            # Record how many islands have we seen
             islands += dfs(matrix, (r,c), visited)
     return islands
 
 def dfs(matrix, current, visited):
-    print("BFS - current: {} visited nodes: {}".format(str(current), visited))
-    Q = []
-    Q.append(current)
-    found_island = 0
-    while Q != []:
-        current = Q.pop(0)
+    """Runs Breadth First Search in the matrix from current coordinates.
+    :param matrix: matrix to be searched.
+    :param current: tuple for coordinates (x,y) to start the algorithm from.
+    :param visited: set of visited coordinates (x,y)."""
+    # print("BFS - current: {} visited nodes: {}".format(str(current), visited))
+    # Create a queue from the starting coordinates.
+    queue = []
+    queue.append(current)
+    found_island = 0  # new islands were found
+    while queue != []:
+        current = queue.pop(0)  # pop from queue
         x,y = current
-        # if matrix[x][y] == [1]:
-        #     print("FOUND A 1")
-        # print("Current in QUEUE " + str(current))
-        # print("IS CURRENT 1? : " + str(matrix[x][y][0]))
+        # Check that the current coordinates have not been visited or != 0
         if current not in visited and matrix[x][y] == [1]:
             visited.add(current)
             found_island = 1
-            print("Found island at: " + str(current))
+            # Find neighbors for current coordinates
             for neighbor in find_neighbors(matrix, current, visited):
-                Q.append(neighbor)
+                queue.append(neighbor)  # Add valid neighbors to queue
     return found_island
 
 # print matrix
@@ -69,12 +87,21 @@ def print_matrix(matrix):
             print(matrix[r][c], end="")
         print()
 
-matrix = [
-    [[0],[1],[1],[0],[1]],
-    [[0],[0],[1],[0],[0]],
-    [[1],[0],[0],[0],[0]],
-    [[0],[0],[0],[1],[1]],
-    [[0],[1],[0],[0],[0]]]
+def main():
+    """Runs getIslands against a matrix."""
 
-is_land = getIslands(matrix)
-print(is_land)
+    # Matrix to be used
+    matrix = [
+        [[0],[1],[1],[0],[1]],
+        [[0],[0],[1],[0],[0]],
+        [[1],[0],[0],[0],[0]],
+        [[0],[0],[0],[1],[1]],
+        [[0],[1],[0],[0],[0]]]
+
+    # Get number of islands of matrix
+    number_of_islands = getIslands(matrix)
+    print("There are {} islands in Matrix:".format(str(number_of_islands)))
+    print_matrix(matrix)
+
+
+main()
